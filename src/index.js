a3ec76444e84171fbefb4e131827898137f65185
TomJ <gh286991@gmail.com>
2022-10-01 18:16:14 +0800

    feat: initial game

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React , {StrictMode} from "react";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
-    <h1>Hello World</h1>,
-  document.getElementById('root')
+import Phaser from "phaser";
+
+import MyGame from "./Phase/MyGame";
+
+const config = {
+  type: Phaser.AUTO,
+  parent: "phaser-example",
+  width: 800,
+  height: 600,
+  scene: [MyGame],
+};
+
+new Phaser.Game(config);
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <React.Fragment>
+      <h1>Hello World</h1>
+    </React.Fragment>
+  </StrictMode>
 );
